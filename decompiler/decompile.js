5cd006f032deb39392021a17b01b47a2a0fb89f1
NighterMan <jpenalbae@gmail.com>
2015-06-01 18:05:01 +0200

    Several improvements

--- decompiler/decompile.js
+++ decompiler/decompile.js
@@ -1,83 +1,99 @@
 #!/usr/bin/env node
 
-var retdec = require('retdec').apiKey('YOUR_API_KEY_HERE');
+var retdec = require('retdec').apiKey('YOUR_API_KEY_KEY_HERE');
 var r2pipe = require('r2pipe');
+var parseArgs = require('minimist');
 
 
-function findFlag(name) 
+var decopts = {};
+
+
+function printHelp()
 {
-    for (var x = flags.length - 1; x >= 0; x--) {
-        if (flags[x].name === name)
-            return flags[x];
-    };
-
-    return null;
+    console.log('\nUsage: $decompile [-achs] [-n naming] @ addr');
+    console.log('  -a: disable selective decompilation (decompile the hole file)');
+    console.log('  -c: clear comments');
+    console.log('  -s: silent. Do not display messages');
+    console.log('  -h: displays this help menu');
+    console.log('  -n naming: select variable naming');
+    console.log('');
+    console.log('Where valid variable namings are:');
+    console.log('  readable: Tries to produce as meaningful variable names as possible');
+    console.log('  address: Variables are named by their addresses in the binary file');
+    console.log('  hungarian: Prefix variables with their type');
+    console.log('  simple: Name variables simply by assigning fruit names');
+    console.log('  unified: Globals, locals and parameters are named just gX, vX and aX');
+    console.log('');
+    console.log('**********************************************************************');
+    console.log('     This will upload the binary being analyzed to retdec.com !!!');
+    console.log('                       You have been warned...');
+    console.log('**********************************************************************\n');
 }
 
 
-function findFunction(code)
+function decompile(options)
 {
-    var split = code.split('\n');
-    var pattern = 'void function_'+ foffset +'(void) {';
-    var found = false;
-
-    console.log(code);
-
-    for (var x = 0; x < split.length; x++) {
-
-        /* Search for function start */
-        if (split[x].indexOf(pattern) !== -1)
-            found = true;
-
-
-        /* Print till end of function */
-        if (found) {
-            console.log(split[x]);
-            if (split[x] === '}')
-                break;
-        }
-
-    }
-
-    if (!found)
-        console.log("Function not found. Sorry :(");
-}
-
-
-function decompile()
-{
-    retdec.decompile(filename, 'bin', {decomp_var_names: 'simple'} , function(err, res) {
+    retdec.decompile(filename, 'bin', options , function(err, res) {
         if (err) {
             console.log('Error (' + err + '): ' + res);
             r2.quit();
             return;
         }
 
-        //findFunction(res.hll.toString());
-        console.log(res.hll.toString());
+        var output = res.hll.toString();
+
+        /* Args: Remove comments */
+        if (args.c)
+            output = output.replace(/^\/.*$\n\n?/mig, '');
+
+        console.log(output);
         r2.quit();
     });
 }
 
 
-
-// if (process.argv.length !== 3) {
-//     console.log('You must suply a function offset');
-//     return 0;
-// }
-
-// var foffset;
-// if (process.argv[2].indexOf('sym.') === 0)
-//     foffset = process.argv[2].substr(4, process.argv[2].length);
-// else
-//     foffset = findFlag(process.argv[2]).offset.toString(16);
-
-
+/* Start r2pipe, get filename and current position */
 var r2 = r2pipe.lpipeSync();
 var filename = r2.cmdj('oj')[0].uri;
-var flags = r2.cmdj('fj');
+var fname = r2.cmd('s');
 
 
-console.log('Please wait for decompilation to finish....\n');
-decompile();
+/* Parse arguments */
+var args = parseArgs(process.argv.slice(2));
+
+/* Args: help */
+if (args.h) {
+    printHelp();
+    process.exit(0);    
+}
+
+/* Args: Selective decompilation */
+if (!args.a) {
+    var fcn = r2.cmdj('pdfj @ ' + fname);
+    if (fcn === null) {
+        console.error('R2 cannot find function at ' + fname);
+        console.error('Try aa/aac or af before running this script');
+        process.exit(1);
+    }
+
+    var startAddr = '0x' + fcn.ops[0].offset.toString(16);
+    var endAddr = '0x' + fcn.ops[fcn.ops.length-1].offset.toString(16);
+    if (!args.s) {
+        console.log("Start: " + startAddr);
+        console.log("End: " + endAddr);
+    }
+    decopts['sel_decomp_ranges'] = startAddr + '-' + endAddr;
+}
+
+/* Args: var naming */
+if (args.n)
+    decopts['decomp_var_names'] = args.n;
+
+
+/* Do the decompilation */
+if (!args.s) {
+    console.log('Uploading binary to retdec.com');
+    console.log('Please wait for decompilation to finish....\n');
+}
+decompile(decopts);
 
