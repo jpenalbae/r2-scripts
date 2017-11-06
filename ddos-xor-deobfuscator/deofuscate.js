8043c518b45e678f159635c2901f71c3ad5876d2
NighterMan <jpenalbae@gmail.com>
2017-11-06 19:52:42 +0100

    Rewrote ddos-xor to use newer r2pipe API

--- ddos-xor-deobfuscator/deofuscate.js
+++ ddos-xor-deobfuscator/deofuscate.js
@@ -1,17 +1,20 @@
+/*
+* This has been rewritten in a rush as the old promise api is no
+* longer available. So code is a mess and a mix of sync and async,
+* es5 and es6 progamming styles, etc...
+*
+* This code should be burned.
+*/
 
+'use strict';
 var r2pipe = require ("r2pipe");
 
-var r2 = null;
-
-var flags = null;
-var info = null;
-
-var encFunc = {
+const encFunc = {
     start: 0,
     end: 0
 }
 
-var decFunc = {
+const decFunc = {
     start: 0,
     end: 0
 }
@@ -19,27 +22,25 @@ var decFunc = {
 
 function decryptAddr(addr, len, start, end, callback)
 {
-    var bytes = info.bin.bits / 8;
+    const bytes = info.bin.bits / 8;
 
     /* 32 bits */
     if ( bytes === 4) {
-        r2.promise(r2.cmd, 'wv ' + addr + ' @ `aer?esp`+4', null)
-             .then(r2.cmd, 'wv ' + len  + ' @ `aer?esp`+8', null)
-             .then(r2.cmd, 'aer eip=' + start, null)
-             .then(r2.cmd, 'aecu ' + end, null)
-             .then(r2.cmd, 'ps @ ' + addr, function(res) { 
-                callback(res);
-             });
+        r2.cmd('wv ' + addr + ' @ `aer?esp`+4');
+        r2.cmd('wv ' + len  + ' @ `aer?esp`+8');
+        r2.cmd('aer eip=' + start);
+        r2.cmd('aecu ' + end);
+        const res = r2.cmd('ps @ ' + addr);
+        callback(res);
 
      /* 64 bits */
      } else if (bytes === 8) {
-        r2.promise(r2.cmd, 'aer rdi=' + addr, null)
-             .then(r2.cmd, 'aer rsi=' + len, null)
-             .then(r2.cmd, 'aer rip=' + start, null)
-             .then(r2.cmd, 'aecu ' + end, null)
-             .then(r2.cmd, 'ps @ ' + addr, function(res) { 
-                callback(res);
-             });
+        r2.cmd('aer rdi=' + addr);
+        r2.cmd('aer rsi=' + len);
+        r2.cmd('aer rip=' + start);
+        r2.cmd('aecu ' + end);
+        const res = r2.cmd('ps @ ' + addr);
+        callback(res);
      }
 }
 
@@ -79,78 +80,68 @@ function decodeDaemon(callback)
     var dF = findFlag('obj.daemonname');
 
     /* Split the daemonflag symbol */
-    r2.cmdj('pcj ' + dF.size + '@ obj.daemonname' , function(res) {
-        var start = dF.offset;
-        var size = 0;
-        var isString = true;
+    let res = r2.cmdj('pcj ' + dF.size + '@ obj.daemonname')
+    let start = dF.offset;
+    let size = 0;
+    let isString = true;
 
-        for (var x = 0; x < res.length; x++) {
+    for (var x = 0; x < res.length; x++) {
 
-            if ((res[x] === 0) && isString) {
-                daemonname.push( { offset: start, size: size } );
-                isString = false;
-            } else if ((res[x] !== 0) && (!isString)) {
-                start = dF.offset + x;
-                isString = true;
-                size = 0;
-            }
+        if ((res[x] === 0) && isString) {
+            daemonname.push( { offset: start, size: size } );
+            isString = false;
+        } else if ((res[x] !== 0) && (!isString)) {
+            start = dF.offset + x;
+            isString = true;
+            size = 0;
+        }
 
-            size++;
-        };
+        size++;
+    };
 
-        console.log('[+] daemonname');
-        printDaemonStrings(daemonname, 0, function() { callback(); })
-        
+    console.log('[+] daemonname');
+    printDaemonStrings(daemonname, 0, function() { callback(); })
 
-    });   
 }
 
 
-
 /* Main code */
-r2pipe.pipe (process.argv[2], function (or2) {
-    r2 = or2;
+const r2 = r2pipe.pipeSync(process.argv[2]);
 
-    /* Initialise radare */
-    r2.promise(r2.cmd, 'aei', null)
-        .then(r2.cmd,  'aeim', null)
-        .then(r2.cmd,  'e io.cache=true', null)
-        .then(r2.cmd,  'aer esp=0x00108000', null)
-        .then(r2.cmd,  'aer rsp=0x00108000', null)
-        .then(r2.cmdj, 'ij', function(data) { info = data; })
-        .then(r2.cmdj, 'fj', function(data) {  flags = data; })
-        .then(r2.cmd,  'af @ sym.encrypt_code', null)
-        .then(r2.cmd,  'af @ sym.decrypt_remotestr', null)
-        .then(r2.cmdj, 'pdfj @ sym.encrypt_code', function(data) {
-            encFunc.start = data.ops[0].offset;
-            encFunc.end = data.ops[data.ops.length-1].offset;
-        })
-        .then(r2.cmdj, 'pdfj @ sym.decrypt_remotestr', function(data) {
+r2.cmd('aei');
+r2.cmd('e io.cache=true');
+r2.cmd('aer esp=0x00108000');
+r2.cmd('aer rsp=0x00108000');
 
-            var encAddr = findFlag('sym.encrypt_code').offset.toString(16);
-            decFunc.start = data.ops[0].offset;
+let info = r2.cmdj('ij');
+let flags = r2.cmdj('fj');
 
-            for (var x = 0; x < data.ops.length; x++) {
-                if (data.ops[x].opcode.indexOf(encAddr) !== -1) {
-                    decFunc.end = data.ops[x+1].offset;
-                    break;
-                }
-            };
-        })
+r2.cmd('af @ sym.encrypt_code');
+r2.cmd('af @ sym.decrypt_remotestr');
 
-        /* Start decrypting */
-        .done(function() {
-            decodeDaemon(function() {
-                var remotestr = findFlag('obj.remotestr');
-                decryptAddr(remotestr.offset, 0, decFunc.start, decFunc.end, function(res) {
-                    console.log('\n[+] remotestr');
-                    var addresses = res.split('|');
-                    for (var i = 0; i < addresses.length; i++) {
-                        console.log('  - ' + addresses[i]);
-                    };
-                    r2.quit();
-                });
-            });
-        });
+let tmpres =  r2.cmdj('pdfj @ sym.encrypt_code');
+encFunc.start = tmpres.ops[0].offset;
+encFunc.end = tmpres.ops[tmpres.ops.length-1].offset;
 
+tmpres = r2.cmdj('pdfj @ sym.decrypt_remotestr');
+//let encAddr = findFlag('sym.encrypt_code').offset.toString(16);
+decFunc.start = tmpres.ops[0].offset;
+for (var x = 0; x < tmpres.ops.length; x++) {
+    if (tmpres.ops[x].opcode.indexOf('sym.encrypt_code') !== -1) {
+        decFunc.end = tmpres.ops[x+1].offset;
+        break;
+    }
+};
+
+decodeDaemon(function() {
+    const remotestr = findFlag('obj.remotestr');
+
+    decryptAddr(remotestr.offset, 0, decFunc.start, decFunc.end, function(res) {
+        console.log('\n[+] remotestr');
+        var addresses = res.split('|');
+        for (var i = 0; i < addresses.length; i++) {
+            console.log('  - ' + addresses[i]);
+        };
+        r2.quit();
+    });
 });
